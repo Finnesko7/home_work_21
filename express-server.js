8df46a068442a93edb098fb9fc389482499171c8
sergey_atamanyuk <sergey.a@uvision.co.il>
2020-04-13 17:40:24 +0300

    home work

--- express-server.js
+++ express-server.js
@@ -18,6 +18,27 @@ app.post('/favorites', function (req, res) {
   res.json(req.body);
 });
 
+app.delete('/favorites/:id', function (req, res) {
+
+    let result = false;
+    let id = req.params.id;
+
+    if (favorites.length > 0) {
+        let index =favorites.findIndex(function (item) {
+            return item.id == id;
+        });
+
+        if (index > -1 ) {
+            favorites.splice(index, 1);
+            result = true;
+        }
+    }
+
+    res.json({
+        'success': result
+    });
+});
+
 app.listen(3000, function () {
   console.log('App is on listening port 3000');
 });
