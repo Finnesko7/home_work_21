730df194158391d0e2083a280749117c0e60928b
sergey_atamanyuk <sergey.a@uvision.co.il>
2020-05-04 18:08:04 +0300

    home work 26

--- express-server.js
+++ express-server.js
@@ -7,42 +7,32 @@ app.use(cors());
 
 const favorites = [];
 
-app.get('/', function (req, res) {
+app.get('/',  (req, res) => {
     res.send('App works!');
 });
 
-app.get('/favorites', function (req, res) {
+app.get('/favorites', (req, res) => {
     res.json(favorites);
 });
 
-app.post('/favorites', function (req, res) {
+app.post('/favorites', (req, res) => {
     favorites.push(req.body);
     res.send({
         success: true
     });
 });
 
-app.delete('/favorites/:id', function (req, res) {
-
-    let result = false;
+app.delete('/favorites/:id', (req, res) => {
     let id = req.params.id;
 
     if (favorites.length > 0) {
-        let index = favorites.findIndex(function (item) {
-            return item.id == id;
-        });
-
-        if (index > -1) {
-            favorites.splice(index, 1);
-            result = true;
-        }
+        var index = favorites.findIndex(item => item.id == id);
+        index > -1 ? favorites.splice(index, 1) : null;
     }
 
     res.json({
-        'success': result
+        'success': index > -1
     });
 });
 
-app.listen(3000, function () {
-    console.log('App is on listening port 3000');
-});
+app.listen(3000,  () => console.log('App is on listening port 3000'));
